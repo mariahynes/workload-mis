81f94fae9d9aed57d581beae1bd5124fe200c1cc
Maria <maria.m.hynes@gmail.com>
2016-07-30 20:18:43 +0100

    updated settings file with new db

--- settings.py
+++ settings.py
@@ -10,8 +10,8 @@ db_config_local = {
 }
 
 db_config = {
-    'db_name': 'heroku_f7ea35f28cc0e1e',
+    'db_name': 'heroku_d577dc995262d7c',
     'host': 'eu-cdbr-west-01.cleardb.com',
-    'user': 'b3c7d2deb89e7f',
-    'pass': 'd47d2a95'
+    'user': 'b3c1d46880d209',
+    'pass': '2cfe375f'
 }
