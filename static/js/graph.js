d48e84c15f2516f5a8637b2207ba7913de1de438
Maria <maria.m.hynes@gmail.com>
2016-07-30 22:39:06 +0100

    update dateformat from %d %B %Y to %d-%b-%y because online db formatted date differently

--- static/js/graph.js
+++ static/js/graph.js
@@ -10,7 +10,7 @@ function makeGraphs(error, projectsJson) {
 
    //Clean projectsJson data
    var teamData = projectsJson;
-   var dateFormat = d3.time.format("%d-%b-%Y");
+   var dateFormat = d3.time.format("%d-%b-%y");
 
    teamData.forEach(function (d) {
 
